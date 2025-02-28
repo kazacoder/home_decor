9a725f633dd0b7a6989963d1242624263c99a0c3
kazacoder <avtk.tlt@gmail.com>
2025-02-28 16:53:16 +0300

    Catalog component logic development is in progress. 1.27

--- src/app/views/product/catalog/catalog.component.ts
+++ src/app/views/product/catalog/catalog.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {ProductService} from "../../../shared/services/product.service";
 import {Router} from "@angular/router";
 import {ProductType} from "../../../../types/product.type";
+import {CategoryService} from "../../../shared/services/category.service";
+import {CategoryWithTypeType} from "../../../../types/category-with-type.type";
 
 @Component({
   selector: 'app-catalog',
@@ -11,15 +13,21 @@ import {ProductType} from "../../../../types/product.type";
 export class CatalogComponent implements OnInit {
 
   products: ProductType[] = []
+  categoriesWithTypes: CategoryWithTypeType[] = []
 
   constructor(private productService: ProductService,
-              private router: Router) { }
+              private router: Router,
+              private categoryService: CategoryService) { }
 
   ngOnInit(): void {
     this.productService.getProducts()
       .subscribe(data => {
         this.products = data.items
       })
+
+    this.categoryService.getCategoriesWithTypes().subscribe(data => {
+      this.categoriesWithTypes = data
+    })
   }
 
 }
