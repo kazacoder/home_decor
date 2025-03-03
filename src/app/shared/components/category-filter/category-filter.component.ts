293ed095323b292fd99d065527a8e2c5ed6dcc45
kazacoder <avtk.tlt@gmail.com>
2025-03-03 15:23:15 +0300

    Catalog component logic development is in progress. Applied filters functional realized

--- src/app/shared/components/category-filter/category-filter.component.ts
+++ src/app/shared/components/category-filter/category-filter.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {CategoryWithTypeType} from "../../../../types/category-with-type.type";
 import {ActivatedRoute, Router} from "@angular/router";
 import {ActiveParamsType} from "../../../../types/active-params.type";
+import {ActiveParamsUtils} from "../../utils/active-params.utils";
 
 @Component({
   selector: 'category-filter',
@@ -35,37 +36,8 @@ export class CategoryFilterComponent implements OnInit {
 
   ngOnInit(): void {
     this.activatedRoute.queryParams.subscribe((params) => {
-      const activeParams: ActiveParamsType = {types: []};
 
-      if (params.hasOwnProperty("types")) {
-        activeParams["types"] = Array.isArray(params['types']) ? params['types'] : [params['types']];
-      }
-
-      if (params.hasOwnProperty("heightFrom")) {
-        activeParams["heightFrom"] = params['heightFrom'];
-      }
-
-      if (params.hasOwnProperty("heightTo")) {
-        activeParams["heightTo"] = params['heightTo'];
-      }
-
-      if (params.hasOwnProperty("diameterFrom")) {
-        activeParams["diameterFrom"] = params['diameterFrom'];
-      }
-
-      if (params.hasOwnProperty("diameterTo")) {
-        activeParams["diameterTo"] = params['diameterTo'];
-      }
-
-      if (params.hasOwnProperty("sort")) {
-        activeParams["sort"] = params['sort'];
-      }
-
-      if (params.hasOwnProperty("page")) {
-        activeParams["page"] = +params['page'];
-      }
-
-      this.activeParams = activeParams
+      this.activeParams = ActiveParamsUtils.processParams(params);
 
       if (this.type) {
         if (this.type === 'height') {
@@ -79,9 +51,10 @@ export class CategoryFilterComponent implements OnInit {
 
         }
       } else {
-        this.activeParams.types = activeParams['types'];
+        this.activeParams.types = this.activeParams['types'];
+        // m13 Catalog time 53:45
         if (this.categoryWithTypes && this.categoryWithTypes.types && this.categoryWithTypes.types.length > 0 &&
-            this.categoryWithTypes.types.some((type) => this.activeParams.types.find(item => type.url === item))) {
+            this.categoryWithTypes.types.some(type => this.activeParams.types.find(item => type.url === item))) {
           this.open = true;
         }
       }
--- src/app/views/product/catalog/catalog.component.ts
+++ src/app/views/product/catalog/catalog.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import {ProductService} from "../../../shared/services/product.service";
-import {Router} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {ProductType} from "../../../../types/product.type";
 import {CategoryService} from "../../../shared/services/category.service";
 import {CategoryWithTypeType} from "../../../../types/category-with-type.type";
+import {ActiveParamsUtils} from "../../../shared/utils/active-params.utils";
+import {ActiveParamsType} from "../../../../types/active-params.type";
+import {AppliedFilterType} from "../../../../types/applied-filter.type";
 
 @Component({
   selector: 'app-catalog',
@@ -12,12 +15,15 @@ import {CategoryWithTypeType} from "../../../../types/category-with-type.type";
 })
 export class CatalogComponent implements OnInit {
 
-  products: ProductType[] = []
-  categoriesWithTypes: CategoryWithTypeType[] = []
+  products: ProductType[] = [];
+  categoriesWithTypes: CategoryWithTypeType[] = [];
+  activeParams: ActiveParamsType = {types: []};
+  appliedFilters: AppliedFilterType[] = []
 
   constructor(private productService: ProductService,
               private router: Router,
-              private categoryService: CategoryService) { }
+              private categoryService: CategoryService,
+              private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.productService.getProducts()
@@ -27,7 +33,65 @@ export class CatalogComponent implements OnInit {
 
     this.categoryService.getCategoriesWithTypes().subscribe(data => {
       this.categoriesWithTypes = data
+
+      this.activatedRoute.queryParams.subscribe(params => {
+        this.activeParams = ActiveParamsUtils.processParams(params);
+
+        this.appliedFilters = [];
+
+        this.activeParams.types.forEach(url => {
+
+          for (let i = 0; i < this.categoriesWithTypes.length; i++) {
+            const foundType = this.categoriesWithTypes[i].types.find(type => type.url === url);
+            if (foundType) {
+              this.appliedFilters.push({
+                name: foundType.name,
+                urlParam: foundType.url,
+              });
+            }
+          }
+        });
+        if (this.activeParams.heightFrom) {
+          this.appliedFilters.push({
+            name: 'Высота: от ' + this.activeParams.heightFrom + ' см',
+            urlParam: 'heightFrom',
+          })
+        }
+        if (this.activeParams.heightTo) {
+          this.appliedFilters.push({
+            name: 'Высота: до ' + this.activeParams.heightTo + ' см',
+            urlParam: 'heightTo',
+          })
+        }
+        if (this.activeParams.diameterFrom) {
+          this.appliedFilters.push({
+            name: 'Диаметр: от ' + this.activeParams.diameterFrom + ' см',
+            urlParam: 'diameterFrom',
+          })
+        }
+        if (this.activeParams.diameterTo) {
+          this.appliedFilters.push({
+            name: 'Диаметр: до ' + this.activeParams.diameterTo + ' см',
+            urlParam: 'diameterTo',
+          })
+        }
+      });
+
     })
+
+
+  }
+
+  removeAppliedFilter(appliedFilter: AppliedFilterType) {
+    if (appliedFilter.urlParam === 'heightFrom' || appliedFilter.urlParam === 'heightTo' ||
+        appliedFilter.urlParam === 'diameterFrom' || appliedFilter.urlParam === 'diameterTo') {
+      delete this.activeParams[appliedFilter.urlParam];
+    } else {
+      this.activeParams.types = this.activeParams.types.filter(item => item !== appliedFilter.urlParam);
+    }
+
+    this.router.navigate(['/catalog'], {queryParams: this.activeParams}).then();
+
   }
 
 }
