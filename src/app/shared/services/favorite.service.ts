0bb4075b4121e0e916c1fb188a63827289da7487
kazacoder <avtk.tlt@gmail.com>
2025-03-06 11:14:19 +0300

    Favorite service finished.

---
+++ src/app/shared/services/favorite.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+import {environment} from "../../../environments/environment";
+import {HttpClient} from "@angular/common/http";
+import {DefaultResponseType} from "../../../types/default-response.type";
+import {FavoriteType} from "../../../types/favorite.type";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FavoriteService {
+
+  constructor(private http: HttpClient) { }
+
+  getFavorites(): Observable<DefaultResponseType | FavoriteType[]> {
+    return this.http.get<DefaultResponseType | FavoriteType[]>(environment.api + 'favorites');
+  }
+
+}
--- src/app/views/personal/favorite/favorite.component.ts
+++ src/app/views/personal/favorite/favorite.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FavoriteService} from "../../../shared/services/favorite.service";
+import {DefaultResponseType} from "../../../../types/default-response.type";
+import {FavoriteType} from "../../../../types/favorite.type";
+import {environment} from "../../../../environments/environment";
 
 @Component({
   selector: 'app-favorite',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavoriteComponent implements OnInit {
 
-  constructor() { }
+  products: FavoriteType[] = [];
+  serverStaticPath = environment.serverStaticPath;
+
+  constructor(private favoriteService: FavoriteService) { }
 
   ngOnInit(): void {
+    this.favoriteService.getFavorites()
+      .subscribe((data: DefaultResponseType | FavoriteType[]) => {
+        if ((data as DefaultResponseType).error !== undefined) {
+          const error = (data as DefaultResponseType).message;
+          throw new Error(error);
+        }
+
+        this.products = data as FavoriteType[];
+        console.log(this.products);
+
+      })
+  }
+
+  removeFromFavorites(id: string): void {
+
   }
 
 }
