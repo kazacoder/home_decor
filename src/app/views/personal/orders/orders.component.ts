c57238d481a2b95e8739ec28e00da0e664ce5929
user <user@test.net>
2025-03-16 16:06:19 +0300

    Orders component finished.

--- src/app/views/personal/orders/orders.component.ts
+++ src/app/views/personal/orders/orders.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {OrderService} from "../../../shared/services/order.service";
 import {OrderType} from "../../../../types/order.type";
 import {DefaultResponseType} from "../../../../types/default-response.type";
+import {OrderStatusUtil} from "../../../shared/utils/order-status.util";
 
 @Component({
   selector: 'app-orders',
@@ -9,7 +10,6 @@ import {DefaultResponseType} from "../../../../types/default-response.type";
   styleUrls: ['./orders.component.scss']
 })
 export class OrdersComponent implements OnInit {
-
   orders: OrderType[] = [];
 
   constructor(private orderService: OrderService) {
@@ -21,7 +21,12 @@ export class OrdersComponent implements OnInit {
         if ((data as DefaultResponseType).error !== undefined) {
           throw new Error((data as DefaultResponseType).message);
         }
-        this.orders = data as OrderType[];
+        this.orders = (data as OrderType[]).map(item => {
+          const status = OrderStatusUtil.getStatusAndColor(item.status)
+          item.statusRus = status.name;
+          item.color = status.color;
+          return item;
+        });
       })
   }
 
--- src/types/order.type.ts
+++ src/types/order.type.ts
@@ -24,4 +24,6 @@ export type OrderType =  {
   }[],
   totalAmount?: number,
   status?: OrderStatusType,
+  statusRus?: string,
+  color?: string,
 }
